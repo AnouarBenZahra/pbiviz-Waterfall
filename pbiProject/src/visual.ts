9b4b2dc13cbb10c49e5f6a4aaeb21e23893b581c
Anouar <anouar.ben.zahra@hotmail.com>
2019-07-28 09:50:46 +0200

    graph

--- pbiProject/src/visual.ts
+++ pbiProject/src/visual.ts
@@ -13,9 +13,13 @@ import DataView = powerbi.DataView;
 import VisualObjectInstanceEnumerationObject = powerbi.VisualObjectInstanceEnumerationObject;
 import { VisualSettings } from "./settings";
 import * as d3 from "d3";
+import { svg } from "d3";
 export interface TestItem {
     Label: string;
     Val: number;
+    start: number;
+    end: number;
+    class: string;
 }
 export class Visual implements IVisual {
 
@@ -74,7 +78,7 @@ export class Visual implements IVisual {
         _this.svg.selectAll('.bar').remove();
         //Draw X Axes
         let xAxis = d3.axisBottom(xScale).
-        tickFormat(function(d){ return d;});
+            tickFormat(function (d) { return d; });
         _this.g
             .append('g')
             .attr('class', 'x axis')
@@ -87,40 +91,146 @@ export class Visual implements IVisual {
             .attr('dy', '-.6em')
             .attr('transform', 'rotate(-90)');
 
-       //Draw Y Axes
-        let yAxis =d3.axisLeft(yScale);
+        //Draw Y Axes
+        let yAxis = d3.axisLeft(yScale);
         _this.g
             .append('g')
             .attr('class', 'y axis')
             .style('fill', 'black')
             .call(yAxis);
 
-    
 
+
+        //**************************************************************************************************** */
+        var cumulative = 0;
+        for (var i = 0; i < dat.length; i++) {
+            dat[i].start = cumulative;
+            cumulative += dat[i].Val;
+            dat[i].end = cumulative;
+
+            dat[i].class = (dat[i].Val >= 0) ? 'positive' : 'negative'
+        }
+        dat.push({
+            Label: 'Total',
+            end: cumulative,
+            start: 0,
+            class: 'total',
+            Val: cumulative
+        });
+
+        xScale.domain(dat.map(function (d) {
+            return d.Label;
+        }));
+        yScale.domain([0, d3.max(dat, function (d) {
+            console.log("end", d.end);
+            return d.end;
+        })]);
+
+        // this.svg.append("g")
+        //     .attr("class", "x axis")
+        //     .attr("transform", "translate(0," + options.viewport.height + ")")
+        //     .call(xAxis);
+
+        var ticks = d3.selectAll(".x.axis text").each(function (d, i) {
+            if (i < 3) {
+                d3.select(this).attr("y", 0)
+                d3.select(this).attr("x", 10)
+                d3.select(this).attr("dy", ".35em")
+                d3.select(this).attr("transform", "rotate(90)")
+                d3.select(this).style("text-anchor", "start");
+            }
+        });
+
+        // this.svg.append("g")
+        //     .attr("class", "y axis")
+        //     .call(yAxis);
+
+        var bar = this.g.selectAll(".bar")
+            .data(dat)
+            .enter().append("g")
+            .attr("class", function (d) {
+                return "bar " + d.class
+            })
+            .attr("transform", function (d) {
+                return "translate(" + xScale(d.Label) + ",0)";
+            });
+
+        bar.append("rect")
+            .attr("y", function (d) {
+                return yScale(Math.max(d.start, d.end));
+            })
+            .attr("height", function (d) {
+                return Math.abs(yScale(d.start) - yScale(d.end));
+            })
+            .attr("width", xScale.bandwidth());
+
+        bar.append("text")
+            .attr("x", xScale.bandwidth() / 2)
+            .attr("y", function (d) {
+                return yScale(d.end) + 5;
+            })
+            .attr("dy", function (d) {
+                return ((d.class == 'negative') ? '-' : '') + ".75em"
+            })
+            .text(function (d) {
+                return Visual.dollarFormatter(d.end - d.start);
+            });
+
+        let padding = 0.3;
+        bar.filter(function (d) {
+            return d.class != "total"
+        }).append("line")
+            .attr("class", "connector")
+            .attr("x1", xScale.bandwidth() + 5)
+            .attr("y1", function (d) {
+                return yScale(d.end)
+            })
+            .attr("x2", xScale.bandwidth() / (1 - padding) - 5)
+            .attr("y2", function (d) {
+                return yScale(d.end)
+            })
+ 
+      
     }
+  
+
     public static converter(options: VisualUpdateOptions): TestItem[] {
 
         let categorical = options.dataViews[0].categorical;
         let category = categorical.categories[0];
         let dataValue = categorical.values[0];
-        // console.log("categorical",categorical);
-        // console.log("category",category);
-        // console.log("dataValue",dataValue);
 
         let rows = categorical.categories[0].values;
-        // console.log("row", rows);
 
         let resultData: TestItem[] = [];
-        //convert from ['x', y] to [{"x":x, "y": y}]
         for (let i = 0; i < rows.length; i++) {
             let row = rows[i];
             console.log(dataValue.values[i]);
             resultData.push({
                 Label: row.toString(),
-                Val: +dataValue.values[i]
+                Val: +dataValue.values[i],
+                class: null,
+                end: null,
+                start: null
             });
         }
         return resultData;
+    }
+    public static percentage(n): number {
+        n = Math.round(n);
+        var result = n;
+        if (Math.abs(n) > 100) {
+            result = Math.round(n / 100) + '%';
+        }
+        return result;
+    }
+    public static dollarFormatter(n) {
+        n = Math.round(n);
+        var result = n;
+        if (Math.abs(n) > 1000) {
+            result = Math.round(n / 1000) + 'K';
+        }
+        return result;
     }
 
     private static parseSettings(dataView: DataView): VisualSettings {
